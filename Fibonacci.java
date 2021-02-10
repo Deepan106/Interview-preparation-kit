a05241a8c8efec24a9b600215237c43c24e9fbe9
Deepan106 <71174419+Deepan106@users.noreply.github.com>
2021-02-10 15:36:30 +0530

    Add files via upload
    fibonacci series using the recursion

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+package traning;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+	 public static int fibonaccii(int n) {
+	        // Complete the function.
+	        if (n <= 1) 
+	            return n;
+	        return fibonaccii(n - 1) 
+	            + fibonaccii(n - 2); 
+	        
+	    }
+
+	    
+
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        int n = scanner.nextInt();
+	        scanner.close();
+	        System.out.println(fibonaccii(n));
+	    }
+
+}
